11ab531ddbb90168593369478f0510c5f41240a8
Alexander Wilhelm Baumgärtner <alexander-wilhelm.baumgaertner@stud.uni-bamberg.de>
2015-12-23 12:19:12 +0100

    Aufgabe 8.2 angefangen

--- src/cacm/CacmSearcher.java
+++ src/cacm/CacmSearcher.java
@@ -7,12 +7,19 @@ import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.DefaultSimilarity;
 import org.apache.lucene.search.similarities.LMDirichletSimilarity;
 import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
 import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.store.FSDirectory;
 
 public class CacmSearcher {
 
@@ -22,8 +29,7 @@ public class CacmSearcher {
 	public static String path2queries = "data/cacm.query.xml";
 
 	// main method for testing
-	public static void main(String[] args) throws IllegalArgumentException,
-			IOException, ParseException {
+	public static void main(String[] args) throws IllegalArgumentException, IOException, ParseException {
 
 		String indexDir = null;// 1
 		String outName = null;
@@ -40,36 +46,41 @@ public class CacmSearcher {
 		}
 
 		// choose the similarity you want
-		Similarity[] sims = new Similarity[] {
-				new LMJelinekMercerSimilarity(0.2f),
-				new LMDirichletSimilarity(), new BM25Similarity(),
-				new DefaultSimilarity() };
+		Similarity[] sims = new Similarity[] { new LMJelinekMercerSimilarity(0.2f), new LMDirichletSimilarity(),
+				new BM25Similarity(), new DefaultSimilarity() };
 
 		// iterate over similarities ... and search
 		for (int i = 0; i < sims.length; i++) {
 			Similarity sim = sims[i];
 			StringBuilder builder = search(indexDir, sim, analyzer);
-			System.err.println("cacm-" + sim.toString() + "-"
-					+ analyzer.toString() + ".trec");
-			FileWriter fw = new FileWriter(new File("logs/cacm-" + sim.toString()
-					+ "-" + outName + ".trec"));
+			System.err.println("cacm-" + sim.toString() + "-" + analyzer.toString() + ".trec");
+			FileWriter fw = new FileWriter(new File("logs/cacm-" + sim.toString() + "-" + outName + ".trec"));
 			fw.write(builder.toString());
 			fw.close();
 		}
 	}
 
 	// implement the search on multiple fields (see exercise 4.3 and following)
-	public static StringBuilder search(String indexDir, Similarity sim,
-			Analyzer analyzer) throws IOException, ParseException {
+	public static StringBuilder search(String indexDir, Similarity sim, Analyzer analyzer)
+			throws IOException, ParseException {
 
 		// read the query texts
 		List<TestQuery> queryList = CacmHelper.readQueries(path2queries);
 		System.out.println("#queries: " + queryList.size());
 
 		StringBuilder builder = null;
-		
+
 		// TODO ab hier bitte implementieren!
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(indexDir).toPath()));
+		IndexSearcher is = new IndexSearcher(reader);
 		
+		
+		for (TestQuery q : queryList) {
+			QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
+			
+			TopDocs hits = is.search(query, 10);
+		}
+
 		return builder;
 	}
 }
